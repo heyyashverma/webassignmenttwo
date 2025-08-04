a4df10b898f3a33c4c32c75eb9f483c0aec7fefc
heyyashverma <heyyashverma.com>
2025-08-04 18:49:05 -0400

    added user auth

---
+++ modules/auth-service.js
@@ -0,0 +1,116 @@
+
+
+const mongoose = require('mongoose');
+require('dotenv').config();
+const bcrypt = require('bcryptjs');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  userName: {
+    type: String,
+    unique: true
+  },
+  password: String,
+  email: String,
+  loginHistory: [
+    {
+      dateTime: Date,
+      userAgent: String
+    }
+  ]
+});
+
+let User;
+
+function initialize() {
+    return new Promise(function (resolve, reject) {
+        let db = mongoose.createConnection(process.env.MONGODB);
+
+        db.on('error', (err) => {
+            reject(err);
+        });
+        db.once('open', () => {
+            User = db.model("users", userSchema);
+            resolve();
+        });
+    });
+}
+
+function registerUser(userData) {
+    return new Promise((resolve, reject) => {
+        if (userData.password !== userData.password2) {
+            reject("Passwords do not match");
+            return;
+        }
+
+        bcrypt.hash(userData.password, 10)
+            .then(hash => {
+                let newUser = new User({
+                    userName: userData.userName,
+                    password: hash,
+                    email: userData.email,
+                    loginHistory: []
+                });
+                newUser.save()
+                    .then(() => {
+                        resolve();
+                    })
+                    .catch((err) => {
+                        if (err.code === 11000) {
+                            reject("User Name already taken");
+                        } else {
+                            reject("There was an error creating the user: " + err);
+                        }
+                    });
+            })
+            .catch(err => {
+                reject("There was an error encrypting the password");
+            });
+    });
+}
+
+function checkUser(userData) {
+    return new Promise((resolve, reject) => {
+        User.find({ userName: userData.userName })
+            .then((users) => {
+                if (users.length === 0) {
+                    reject("Unable to find user: " + userData.userName);
+                } else {
+                    bcrypt.compare(userData.password, users[0].password)
+                        .then((result) => {
+                            if (!result) {
+                                reject("Incorrect Password for user: " + userData.userName);
+                            } else {
+                                if (users[0].loginHistory.length === 8) {
+                                    users[0].loginHistory.pop();
+                                }
+                                users[0].loginHistory.unshift({
+                                    dateTime: (new Date()).toString(),
+                                    userAgent: userData.userAgent
+                                });
+
+                                User.updateOne(
+                                    { userName: users[0].userName },
+                                    { $set: { loginHistory: users[0].loginHistory } }
+                                )
+                                    .then(() => {
+                                        resolve(users[0]);
+                                    })
+                                    .catch((err) => {
+                                        reject("There was an error verifying the user: " + err);
+                                    });
+                            }
+                        })
+                        .catch(() => {
+                            reject("Incorrect Password for user: " + userData.userName);
+                        });
+                }
+            })
+            .catch(() => {
+                reject("Unable to find user: " + userData.userName);
+            });
+    });
+}
+
+module.exports = { initialize, registerUser, checkUser };
--- server.js
+++ server.js
@@ -14,7 +14,10 @@
 
 const express = require("express");
 const path = require("path");
+
 const projectData = require("./modules/projects");
+const authData = require("./modules/auth-service");
+const clientSessions = require("client-sessions");
 
 const app = express();
 const HTTP_PORT = process.env.PORT || 8080;
@@ -24,154 +27,236 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 // Middleware
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 
+// Configure client-sessions middleware
+app.use(clientSessions({
+  cookieName: "session",
+  secret: "web322_assignment5_secret_key",
+  duration: 20 * 60 * 1000, // 20 minutes
+  activeDuration: 5 * 60 * 1000 // 5 minutes
+}));
+
+// Expose session to all templates
+app.use((req, res, next) => {
+  res.locals.session = req.session;
+  next();
+});
+
+// Helper middleware to ensure login
+function ensureLogin(req, res, next) {
+  if (!req.session.user) {
+    return res.redirect("/login");
+  }
+  next();
+}
+
 // Initialize and start server
-projectData.initialize().then(() => {
-  console.log("Project data initialized successfully.");
 
-  // Home
-  app.get("/", (req, res) => {
-    res.render("home", { page: "/" });
-  });
+projectData.initialize()
+  .then(authData.initialize)
+  .then(() => {
+    console.log("Project data and auth data initialized successfully.");
 
-  // About
-  app.get("/about", (req, res) => {
-    res.render("about", { page: "/about" });
-  });
+    // Home
+    app.get("/", (req, res) => {
+      res.render("home", { page: "/" });
+    });
 
-  // All Projects or Filtered by Sector
-  app.get("/solutions/projects", (req, res) => {
-    const sector = req.query.sector;
+    // About
+    app.get("/about", (req, res) => {
+      res.render("about", { page: "/about" });
+    });
 
-    const renderPage = (projects) => {
-      if (projects.length > 0) {
-        res.render("projects", { projects, page: "/solutions/projects" });
-      } else {
-        res.status(404).render("404", {
+    // All Projects or Filtered by Sector
+    app.get("/solutions/projects", (req, res) => {
+      const sector = req.query.sector;
+
+      const renderPage = (projects) => {
+        if (projects.length > 0) {
+          res.render("projects", { projects, page: "/solutions/projects" });
+        } else {
+          res.status(404).render("404", {
+            page: "",
+            message: `No projects found${sector ? ' for sector: ' + sector : '.'}`
+          });
+        }
+      };
+
+      const handleError = (err) => {
+        console.error(err);
+        res.status(500).render("404", {
           page: "",
-          message: `No projects found${sector ? ' for sector: ' + sector : '.'}`
+          message: `Error fetching projects: ${err}`
+        });
+      };
+
+      if (sector) {
+        projectData.getProjectsBySector(sector).then(renderPage).catch(handleError);
+      } else {
+        projectData.getAllProjects().then(renderPage).catch(handleError);
+      }
+    });
+
+    // Single Project by ID
+    app.get("/solutions/projects/:id", (req, res) => {
+      const projectId = parseInt(req.params.id, 10);
+      if (isNaN(projectId)) {
+        return res.status(400).render("404", {
+          page: "",
+          message: "Invalid project ID."
         });
       }
-    };
 
-    const handleError = (err) => {
-      console.error(err);
-      res.status(500).render("404", {
-        page: "",
-        message: `Error fetching projects: ${err}`
-      });
-    };
-
-    if (sector) {
-      projectData.getProjectsBySector(sector).then(renderPage).catch(handleError);
-    } else {
-      projectData.getAllProjects().then(renderPage).catch(handleError);
-    }
-  });
-
-  // Single Project by ID
-  app.get("/solutions/projects/:id", (req, res) => {
-    const projectId = parseInt(req.params.id, 10);
-    if (isNaN(projectId)) {
-      return res.status(400).render("404", {
-        page: "",
-        message: "Invalid project ID."
-      });
-    }
-
-    projectData.getProjectById(projectId)
-      .then(project => {
-        res.render("project", { project, page: "" });
-      })
-      .catch(err => {
-        res.status(404).render("404", { page: "", message: err });
-      });
-  });
-
-  // Add Project (GET)
-  app.get("/solutions/addProject", (req, res) => {
-    projectData.getAllSectors()
-      .then(sectors => {
-        res.render("addProject", { sectors, page: "/solutions/addProject" });
-      })
-      .catch(err => {
-        res.render("500", {
-          message: `I'm sorry, but we have encountered the following error: ${err}`
+      projectData.getProjectById(projectId)
+        .then(project => {
+          res.render("project", { project, page: "" });
+        })
+        .catch(err => {
+          res.status(404).render("404", { page: "", message: err });
         });
-      });
-  });
+    });
 
-  // Add Project (POST)
-  app.post("/solutions/addProject", (req, res) => {
-    projectData.addProject(req.body)
-      .then(() => {
-        res.redirect("/solutions/projects");
-      })
-      .catch(err => {
-        res.render("500", {
-          message: `I'm sorry, but we have encountered the following error: ${err}`
-        });
-      });
-  });
-
-  // Edit Project (GET)
-  app.get("/solutions/editProject/:id", (req, res) => {
-    const projectId = parseInt(req.params.id, 10);
-
-    Promise.all([
-      projectData.getProjectById(projectId),
+    // Add Project (GET)
+    app.get("/solutions/addProject", ensureLogin, (req, res) => {
       projectData.getAllSectors()
-    ])
-      .then(([project, sectors]) => {
-        res.render("editProject", { project, sectors, page: "" });
-      })
-      .catch(err => {
-        res.status(404).render("404", { message: err });
-      });
-  });
-
-  // Edit Project (POST)
-  app.post("/solutions/editProject", (req, res) => {
-    const projectId = parseInt(req.body.id, 10);
-
-    projectData.editProject(projectId, req.body)
-      .then(() => {
-        res.redirect("/solutions/projects");
-      })
-      .catch(err => {
-        res.render("500", {
-          message: `I'm sorry, but we have encountered the following error: ${err}`
+        .then(sectors => {
+          res.render("addProject", { sectors, page: "/solutions/addProject" });
+        })
+        .catch(err => {
+          res.render("500", {
+            message: `I'm sorry, but we have encountered the following error: ${err}`
+          });
         });
-      });
-  });
+    });
 
-  // Delete Project
-  app.get("/solutions/deleteProject/:id", (req, res) => {
-    const projectId = parseInt(req.params.id, 10);
-
-    projectData.deleteProject(projectId)
-      .then(() => {
-        res.redirect("/solutions/projects");
-      })
-      .catch(err => {
-        res.render("500", {
-          message: `I'm sorry, but we have encountered the following error: ${err}`
+    // Add Project (POST)
+    app.post("/solutions/addProject", ensureLogin, (req, res) => {
+      projectData.addProject(req.body)
+        .then(() => {
+          res.redirect("/solutions/projects");
+        })
+        .catch(err => {
+          res.render("500", {
+            message: `I'm sorry, but we have encountered the following error: ${err}`
+          });
         });
-      });
-  });
+    });
 
-  // Catch-all 404
-  app.use((req, res) => {
-    res.status(404).render("404", { page: "", message: "Page not found." });
-  });
+    // Edit Project (GET)
+    app.get("/solutions/editProject/:id", ensureLogin, (req, res) => {
+      const projectId = parseInt(req.params.id, 10);
 
-  // Start server
-  app.listen(HTTP_PORT, () => {
-    console.log(`Server listening on port ${HTTP_PORT}`);
-  });
+      Promise.all([
+        projectData.getProjectById(projectId),
+        projectData.getAllSectors()
+      ])
+        .then(([project, sectors]) => {
+          res.render("editProject", { project, sectors, page: "" });
+        })
+        .catch(err => {
+          res.status(404).render("404", { message: err });
+        });
+    });
 
-}).catch(err => {
-  console.error("Failed to initialize project data or start server:", err);
-});
+    // Edit Project (POST)
+    app.post("/solutions/editProject", ensureLogin, (req, res) => {
+      const projectId = parseInt(req.body.id, 10);
+
+      projectData.editProject(projectId, req.body)
+        .then(() => {
+          res.redirect("/solutions/projects");
+        })
+        .catch(err => {
+          res.render("500", {
+            message: `I'm sorry, but we have encountered the following error: ${err}`
+          });
+        });
+    });
+
+    // Delete Project
+    app.get("/solutions/deleteProject/:id", ensureLogin, (req, res) => {
+      const projectId = parseInt(req.params.id, 10);
+
+      projectData.deleteProject(projectId)
+        .then(() => {
+          res.redirect("/solutions/projects");
+        })
+        .catch(err => {
+          res.render("500", {
+            message: `I'm sorry, but we have encountered the following error: ${err}`
+          });
+        });
+    });
+
+
+    // GET /login
+
+    app.get("/login", (req, res) => {
+      res.render("login", { errorMessage: "", userName: "", page: "/login" });
+    });
+
+    // GET /register
+
+    app.get("/register", (req, res) => {
+      res.render("register", { errorMessage: "", successMessage: "", userName: "", page: "/register" });
+    });
+
+    // POST /register
+
+    app.post("/register", (req, res) => {
+      authData.registerUser(req.body)
+        .then(() => {
+          res.render("register", { errorMessage: "", successMessage: "User created", userName: "", page: "/register" });
+        })
+        .catch((err) => {
+          res.render("register", { errorMessage: err, successMessage: "", userName: req.body.userName, page: "/register" });
+        });
+    });
+
+    // POST /login
+
+    app.post("/login", (req, res) => {
+      req.body.userAgent = req.get('User-Agent');
+      authData.checkUser(req.body)
+        .then((user) => {
+          req.session.user = {
+            userName: user.userName,
+            email: user.email,
+            loginHistory: user.loginHistory
+          };
+          res.redirect('/solutions/projects');
+        })
+        .catch((err) => {
+          res.render("login", { errorMessage: err, userName: req.body.userName, page: "/login" });
+        });
+    });
+
+    // GET /logout
+    app.get("/logout", (req, res) => {
+      req.session.reset();
+      res.redirect('/');
+    });
+
+    // GET /userHistory (protected)
+
+    app.get("/userHistory", ensureLogin, (req, res) => {
+      res.render("userHistory", { page: "/userHistory" });
+    });
+
+    // Catch-all 404
+    app.use((req, res) => {
+      res.status(404).render("404", { page: "", message: "Page not found." });
+    });
+
+    // Start server
+    app.listen(HTTP_PORT, () => {
+      console.log(`Server listening on port ${HTTP_PORT}`);
+    });
+
+  })
+  .catch(err => {
+    console.error("Failed to initialize project data or auth data or start server:", err);
+  });
